551df1c3e0ca9d2dc740881958fcba77321d4a90
Santiago <santiagocantu98@gmail.com>
2020-03-01 19:57:38 -0600

    The work done in the Ember Course

---
+++ library-ui/app/models/book.js
@@ -0,0 +1,13 @@
+import DS from 'ember-data';
+const { Model } = DS;
+
+export default Model.extend({
+    title: DS.attr(),
+    isbn: DS.attr(),
+    publishDate: DS.attr(),
+    username: DS.attr(),
+
+    author: DS.belongsTo('author'),
+    reviews: DS.hasMany('review')
+
+});
---
+++ library-ui/app/routes/book.js
@@ -0,0 +1,20 @@
+import Route from '@ember/routing/route';
+
+export default Route.extend({
+    queryParams: {
+        search: {
+            refreshModel: true
+        }
+    },
+
+    model( { search }) {
+        if (search) {
+            return this.store.query('book', {
+                filter: { query: search}
+            });
+        }
+
+        return this.store.findAll('book');
+
+    }
+});
